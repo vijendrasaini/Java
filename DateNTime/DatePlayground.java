ddd25b6a8fdd873842d154a4036ba9445a69d24e
bhartraj saini <bhartrajsaini@861gmail,com>
2026-06-10 14:55:34 +0530

    covered Period class

--- DateNTime/DatePlayground.java
+++ DateNTime/DatePlayground.java
@@ -11,5 +11,12 @@ public class DatePlayground {
         System.out.println(now);
         LocalDate joiningDate = LocalDate.of(2022, 5, 6);
         System.out.println(joiningDate);
+
+        // Period
+        Period experiance = Period.between(joiningDate, now);
+        System.out.println(experiance);
+        System.out.println("%d day(s), %d month(s), %d year(s)".formatted(experiance.getDays(), experiance.getMonths(), experiance.getYears()));
+        System.out.println(experiance.getChronology());
+        System.out.println(experiance.getUnits());
     }
 }
